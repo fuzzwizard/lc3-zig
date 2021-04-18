142103bdc0c53e5e44fcfa92d10a7df923cfde0d
fuzzwizard <momojak@gmail.com>
2021-04-17 22:03:16 -0700

    get to ch. 6

--- src/main.zig
+++ src/main.zig
@@ -38,17 +38,32 @@ const Flag = enum(u16) {
     NEG = 1 << 2,
 };
 
-pub fn main() anyerror!void {
-    var memory = [_]u16{0} ** std.math.maxInt(u16);
-    var regs = [_]u16{0} ** @typeInfo(Register).Enum.fields.len;
+fn read_image(path: ?[]const u8) anyerror!bool {
+    if (path) |p| {
+        return true;
+    }
+    return error.NoPathProvided;
+}
 
-    const pc_start = 0x300;
-    regs[@enumToInt(Register.PC)] = pc_start;
+var memory = [_]u16{0} ** std.math.maxInt(u16);
+var regs = [_]u16{0} ** @typeInfo(Register).Enum.fields.len;
+
+fn get_reg(r: Register) u16 {
+    return regs[@enumToInt(r)];
+}
+
+fn get_reg_ptr(r: Register) *u16 {
+    return &regs[@enumToInt(r)];
+}
+
+fn lc3() !void {
+    const pc_start = 0x3000;
+    get_reg_ptr(.PC).* = pc_start;
 
     var running = true;
     while (running) {
-        var instr = memory[regs[@enumToInt(Register.PC)]];
-        regs[@enumToInt(Register.PC)] += 1;
+        var instr = memory[get_reg(.PC)];
+        get_reg_ptr(.PC).* += 1;
         var op = instr >> 12;
         switch (@intToEnum(Opcode, op)) {
             .BR => unreachable,
@@ -59,16 +74,36 @@ pub fn main() anyerror!void {
             .AND => unreachable,
             .LDR => unreachable,
             .STR => unreachable,
-            .RTI => unreachable,
             .NOT => unreachable,
             .LDI => unreachable,
             .STI => unreachable,
             .JMP => unreachable,
-            .RES => unreachable,
             .LEA => unreachable,
             .TRAP => unreachable,
-            else => {},
+            .RTI, .RES => return error.BadOpCode,
         }
         running = false;
     }
+}
+
+pub fn main() anyerror!void {
+    const al = std.heap.page_allocator;
+
+    const args = try std.process.argsAlloc(al);
+    defer std.process.argsFree(al, args);
+
+    if (args.len < 2) {
+        std.debug.warn("lc3 [image-file1] ...\n", .{});
+        return error.InsufficientArgs;
+    }
+    // TODO: the catch here isn't detecting that read_image has the return type of `!anything`
+    // so it gets mad that we try to catch it. Probably worth a bug report, if consistently reproducable.
+    // for (args[1..]) |arg| {
+    //     try read_image(arg) catch |e| { // TODO: why doesn't this work?
+    //         std.debug.warn("Failed to load file: {}", .{arg});
+    //         return e;
+    //     };
+    // }
+
+    try lc3();
 }
