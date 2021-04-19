3d79be333b5eaec461f885a6ad5587b1259c6fc2
fuzzwizard <momojak@gmail.com>
2021-04-18 21:21:44 -0700

    finish up ch. 6

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 
-// NOTE: Don't reorder these.
 const Register = enum(u16) {
     R0,
     R1,
@@ -12,68 +11,94 @@ const Register = enum(u16) {
     R7,
     PC,
     COND,
+
+    const Self = @This();
+
+    pub fn as_u16(self: Self) u16 {
+        return @enumToInt(self);
+    }
+
+    pub fn get_count() usize {
+        return @typeInfo(Register).Enum.fields.len;
+    }
 };
 
-// NOTE: Don't reorder these.
 const Opcode = enum(u16) {
-    BR = 0, // branch
-    ADD, // add
-    LD, // load
-    ST, // store
-    JSR, // jump register
-    AND, // bitwise and
-    LDR, // load register
-    STR, // store register
-    RTI, // unused
-    NOT, // bitwise not
-    LDI, // load indirect
-    STI, // store indirect
-    JMP, // jump
-    RES, // reserved (unused)
-    LEA, // load effective address
-    TRAP // execute trap
+    BR = 0b0000, // branch
+    ADD = 0b0001, // add:
+    //  [0001|DR |SR1|0|00|SR2] (register)
+    //  [0001|DR |SR1|1| IMM5 ] (immediate s5 value)
+    LD = 0b0010, // load
+    ST = 0b0011, // store
+    JSR = 0b0100, // jump register
+    AND = 0b0101, // bitwise and
+    LDR = 0b0110, // load register
+    STR = 0b0111, // store register
+    RTI = 0b1000, // unused
+    NOT = 0b1001, // bitwise not
+    LDI = 0b1010, // load indirect [1010|DR |OFFSET   ]
+    STI = 0b1011, // store indirect
+    JMP = 0b1100, // jump
+    RES = 0b1101, // reserved (unused)
+    LEA = 0b1110, // load effective address
+    TRAP = 0b1111, // execute trap
+
+    const Self = @This();
+    pub fn as_u16(self: Self) u16 {
+        return @enumToInt(self);
+    }
 };
 
 const Flag = enum(u16) {
     POS = 1 << 0,
     ZRO = 1 << 1,
     NEG = 1 << 2,
+
+    const Self = @This();
+    pub fn as_u16(self: Self) u16 {
+        return @enumToInt(self);
+    }
 };
 
 fn read_image(path: ?[]const u8) !void {
     if (path) |p| {
+        // UNIMPLEMENTED
         return;
     }
     return error.NoPathProvided;
 }
 
 var memory = [_]u16{0} ** std.math.maxInt(u16);
-var regs = [_]u16{0} ** @typeInfo(Register).Enum.fields.len;
+var reg = [_]u16{0} ** Register.get_count();
+
+inline fn update_flags(r0: u16) void {
+    if (reg[r] == 0) {
+        set_reg(.COND, Flag.ZRO.as_u16());
+    } else if (reg[r] >> 15 != 0) {
+        set_reg(.COND, Flag.NEG.as_u16());
+    } else {
+        set_reg(.COND, Flag.POS.as_u16());
+    }
+}
 
 inline fn set_reg(r: Register, val: u16) void {
-    regs[@enumToInt(r)] = val;
+    reg[r.as_u16()] = val;
 }
 
 inline fn get_reg(r: Register) u16 {
-    return regs[@enumToInt(r)];
+    return reg[r.as_u16()];
 }
 
-inline fn update_flags(r: Register) void {
-    if (get_reg(r) == 0) {
-        set_reg(.COND, @enumToInt(Flag.ZRO));
-    } else if (get_reg(r) >> 15 != 0) {
-        set_reg(.COND, @enumToInt(Flag.NEG));
-    } else {
-        set_reg(.COND, @enumToInt(Flag.POS));
-    }
+inline fn mem_read(addr: u16) u16 {
+    return memory[addr];
 }
 
 inline fn sign_extend(x: u16, bit_count: u4) u16 {
-    var ret = x;
-    if ((ret >> (bit_count - 1)) & 0x1 != 0) {
-        ret |= @as(u16, 0xFFFF) << bit_count;
+    if ((x >> (bit_count - 1)) & 0b1 != 0) {
+        return x | @as(u16, 0xFFFF) << bit_count;
+    } else {
+        return x;
     }
-    return ret;
 }
 
 fn lc3() !u16 {
@@ -86,20 +111,26 @@ fn lc3() !u16 {
         set_reg(.PC, get_reg(.PC) + 1);
         var op = @intToEnum(Opcode, instr >> 12);
         switch (op) {
-            .BR => {},
             .ADD => {
                 const r0 = (instr >> 9) & 0x7;
                 const r1 = (instr >> 6) & 0x7;
                 const imm_flag = (instr >> 5) & 0x1;
                 if (imm_flag == 1) {
-                    const imm5 = sign_extend(instr & 0b11111, 5);
-                    set_reg(.R0, get_reg(.R1) + imm5);
+                    const imm5 = sign_extend(instr & 0b1_1111, 5);
+                    reg[r0] = reg[r1] + imm5;
                 } else {
                     const r2 = instr & 0x7;
-                    set_reg(.R0, get_reg(.R1) + regs[r2]);
+                    reg[r0] = reg[r1] + reg[r2];
                 }
-                update_flags(.R0);
+                update_flags(r0);
             },
+            .LDI => {
+                const r0 = (instr >> 9) & 0x7;
+                const pc_offset = sign_extend(instr & 0b1_1111_1111, 9);
+                reg[r0] = mem_read(mem_read(get_reg(.PC) + pc_offset));
+                update_flags(r0);
+            },
+            .BR => {},
             .LD => {},
             .ST => {},
             .JSR => {},
@@ -107,7 +138,6 @@ fn lc3() !u16 {
             .LDR => {},
             .STR => {},
             .NOT => {},
-            .LDI => {},
             .STI => {},
             .JMP => {},
             .LEA => {},
@@ -133,7 +163,7 @@ pub fn main() anyerror!void {
 
     // for (args[1..]) |arg| {
     //     try read_image(arg) catch |e| {
-    // TODO: the catch here isn't detecting that read_image has the return type of `!void`	
+    // TODO: the catch here isn't detecting that read_image has the return type of `!void`
     // so it gets mad that we try to catch it. Probably worth a bug report, if consistently reproducable. (Zig 7.1.0)
     //         std.debug.warn("Failed to load file: {}", .{arg});
     //         return e;
